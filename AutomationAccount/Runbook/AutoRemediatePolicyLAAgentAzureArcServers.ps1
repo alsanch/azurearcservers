9e406ac436c36e4a1e67a53cd5c3fce0297ceb10
alejandro <alsanch@microsoft.com>
2021-12-15 22:32:30 +0100

    Initial Commit

---
+++ AutomationAccount/Runbook/AutoRemediatePolicyLAAgentAzureArcServers.ps1
@@ -0,0 +1,49 @@
+<#
+    .DESCRIPTION
+        Remediate Azure Policy "Configure Log Analytics extension on Azure Arc enabled Windows servers" with PowerShell 
+        Based on https://adatum.no/azure/azure-policy/remediate-azure-policy-with-powershell
+
+    .NOTES
+        AUTHOR: Alejandro Sanchez Gomez
+        LASTEDIT: Nov 22, 2021
+#>
+
+"Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
+
+try
+{
+    "Logging in to Azure..."
+    Connect-AzAccount -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+
+# Definition name for policy Configure Log Analytics extension on Azure Arc enabled Windows/Linux servers
+$policyDefinitionNames = @('69af7d4a-7b18-4044-93a9-2651498ef203','9d2b61b4-1d14-4a63-be30-d4498e7ad2cf')
+
+# get all non-compliant policies that can be remediated
+$nonCompliantPolicies = Get-AzPolicyState | Where-Object { $_.ComplianceState -eq "NonCompliant" -and $_.PolicyDefinitionName -in $policyDefinitionNames -and $_.PolicyDefinitionAction -eq "deployIfNotExists" }
+
+# loop through and start individual tasks per policy 
+foreach ($policy in $nonCompliantPolicies) {
+
+    $remediationName = "rem." + $policy.PolicyDefinitionName
+    
+    # Policy assigned at RG level -- Remedation done at RG level
+    if($policy.PolicyAssignmentId -like "*resourcegroups*"){
+        $scope = $policy.PolicyAssignmentId.Split("/")[4]
+        Start-AzPolicyRemediation -Name $remediationName -ResourceGroupName $scope -PolicyAssignmentId $policy.PolicyAssignmentId -ResourceDiscoveryMode ReEvaluateCompliance
+    }
+    # Policy assigned at MG level -- Remedation done at MG level
+    elseif($policy.PolicyAssignmentId -like "*managementGroups*") {
+        $scope = $policy.PolicyAssignmentId.Split("/")[4]
+        Start-AzPolicyRemediation -Name $remediationName -ManagementGroupName $scope -PolicyAssignmentId $policy.PolicyAssignmentId -ResourceDiscoveryMode ReEvaluateCompliance
+    
+    }
+    # Policy assigned at subscription level -- Remedation done at subscription level
+    else {
+        Start-AzPolicyRemediation -Name $remediationName -PolicyAssignmentId $policy.PolicyAssignmentId -ResourceDiscoveryMode ReEvaluateCompliance
+    }
+}
---
+++ DeployAzureArcMVP.ps1
@@ -0,0 +1,291 @@
+# Deployment Variables to choose what to deploy
+$deployMonitorLAW = $true
+$deployMonitorLAWDataSources = $true
+$deployAutomationAccount = $true
+$deployActionGroup = $true
+$deployAlerts = $true
+$deployWorkbooks = $true
+$deployDashboard = $true
+$deployAzurePolicies = $true
+$deployDefenderForCloud = $true
+
+# Global variables
+$parametersFilePath = ".\Parameters.csv"
+$parametersFileInput = $(Import-Csv $parametersFilePath)
+$subscriptionName = $parametersFileInput.Subscription
+$resourceGroup = $parametersFileInput.ResourceGroup
+$namingPrefix = $parametersFileInput.NamingPrefix
+$location = $parametersFileInput.Location
+$MonitorWSName = $namingPrefix + "-la-monitor"
+$SecurityWSName = $namingPrefix + "-la-security"
+$ActionGroupName = $namingPrefix + "-ag-arc"
+$ActionGroupShortName = $namingPrefix + "-agarc"
+$AutomationAccountName = $namingPrefix + "-aa-arcservers"
+
+# Option to interrupt the deployment  
+Write-Host -ForegroundColor Green "STARTING THE DEPLOYMENT"
+Write-Host -ForegroundColor Red "NOTE: Press CTRL+C within 10 seconds to cancel the deployment"
+Start-Sleep -Seconds 10 
+
+# Login to Azure
+if ($(Get-AzContext).Name -eq "Default")
+{
+    Login-AzAccount  
+}
+
+# Switch Context to designated subscription if needed
+if((Get-AzContext).Subscription.Name -ne $subscriptionName)
+{
+    Select-AzSubscription -SubscriptionName $subscriptionName | Out-Null
+}
+
+# Create the ResourceGroup if needed
+Get-AzResourceGroup -Name $resourceGroup -ErrorVariable notPresent -ErrorAction SilentlyContinue | Out-Null
+if ($notPresent)
+{
+    Write-Host "Creating resource group $resourceGroup"
+    New-AzResourceGroup -Name $resourceGroup -Location $location | Out-Null
+}
+
+### Deploy the Monitor Log Analytics workspace
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying the Monitor Log Analytics Workspace"
+if($deployMonitorLAW -eq $true)
+{
+    $deploymentName = "Deploy_$($parametersFileInput.MonitorWSName)"
+    $templateFile = ".\MonitorLogAnalyticsWorkspace\deploy.json"
+        
+    # Deploy the workspace
+    Write-Host "Deploying log analytics workspace $MonitorWSName"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+    -workspaceName $MonitorWSName -location $location | Out-Null
+    
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Enable LA Monitor Data Sources: Events, Performance Counters, etc
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying Data Sources in the Monitor the Log Analytics Workspace"
+if($deployMonitorLAWDataSources -eq $true)
+{
+    $deploymentName = "Deploy_DataSources"
+    $templateFile = ".\MonitorLogAnalyticsWorkspace\DataSources\deploy.json"
+
+    # Deploy the data sources
+    Write-Host "Deploying data sources for $MonitorWSName"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+    -workspaceName $MonitorWSName | Out-Null
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Automation account related resources
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying the Automation Account and related resources"
+if($deployAutomationAccount -eq $true)
+{
+    $deploymentName = "AutomationAccount"
+    $templateFile = ".\AutomationAccount\deploy.json"
+    $managedIdentityScope = $parametersFileInput.Scope
+
+    # Deploy and link the automation account
+    Write-Host "Deploying and linking automation account to $MonitorWSName"
+    Write-Host "Deploying Update Management, Change Tracking and Inventory in the automation account $automationAccountName"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+        -workspaceName $MonitorWSName -automationAccountName $automationAccountName -location $location | Out-Null
+
+    # Create and publish the runbook
+    $runbookName = "AutoRemediatePolicyLAAgentAzureArcServers"
+    $runbookType = "PowerShell"
+    $runbookCodePath = ".\AutomationAccount\Runbook\AutoRemediatePolicyLAAgentAzureArcServers.ps1"
+    Write-Host "Importing the required runbooks"
+    Import-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroup  -Name $runbookName `    -Type $runbookType -Path $runbookCodePath | Out-null 
+           
+    Write-Host "Publishing the required runbooks"
+    Publish-AzAutomationRunbook -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroup -Name $runbookName | Out-null
+
+    # Create and link the schedule
+    Write-Host "Creating the daily schedule and linking it to the runbook"
+    $StartTime = Get-Date "23:00:00"
+    $EndTime = $StartTime.AddYears(99)
+    $scheduleName = "dailyschedule11pm"
+    $TimeZone = ([System.TimeZoneInfo]::Local).Id
+    New-AzAutomationSchedule -AutomationAccountName $automationAccountName -Name $scheduleName -StartTime $StartTime -ExpiryTime `
+    $EndTime -DayInterval 1 -ResourceGroupName $resourceGroup -TimeZone $TimeZone | Out-null
+
+    Register-AzAutomationScheduledRunbook -AutomationAccountName $automationAccountName `
+    -Name $runbookName -ScheduleName $scheduleName -ResourceGroupName $resourceGroup | Out-null
+
+    # Get automation account managed identity and assign permissions to remediate policies at subscription level
+    $principalId = (Get-AzAutomationAccount -ResourceGroupName $resourceGroup -Name $automationAccountName).Identity.PrincipalId
+    if($managedIdentityScope -eq "subscription")
+    {
+        New-AzRoleAssignment -ObjectId $principalId -RoleDefinitionName "Resource Policy Contributor" | Out-null
+    }
+    elseif($managedIdentityScope -eq "resourcegroup") {
+        New-AzRoleAssignment -ObjectId $principalId -RoleDefinitionName "Resource Policy Contributor" `
+        -ResourceGroupName $resourceGroup | Out-null
+    }
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Create Azure Monitor action group with an email address
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying Azure Monitor Action Group"
+if($deployActionGroup -eq $true)
+{
+    $deploymentName = "Deploy_ActionGroup"
+    $templateFile = ".\ActionGroup\deploy.json"
+    $emailAddress = $parametersFileInput.Email
+        
+    # Deploy the data sources
+    Write-Host "Deploying Azure Monitor Action Group"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+    -actionGroupName $actionGroupName -actionGroupShortName $actionGroupShortName -emailAddress $emailAddress | Out-Null
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Set up the alert baseline
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying Azure Monitor alerts"
+if($deployAlerts -eq $true)
+{         
+    $templateBasePath = ".\Alerts"
+
+    # Get the alerts ARM template files
+    $alertCollection = $(ls -Path $templateBasePath | Where-Object {$_.name -like "*.json"})
+        
+    # Deploy all the alerts
+    foreach ($alert in $alertCollection)
+    {
+        $alertName = $($alert.Name).Split(".")[0]
+        $templateFile = "$templateBasePath\$($alert.Name)"
+        $deploymentName = $("import_alert_$alertName").ToLower()
+
+        # Deploy this alert
+        Write-Host "Deploying alert: $alertName"
+        New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+        -workspaceName $MonitorWSName -location $location -actionGroupName $actionGroupName | Out-Null
+    }
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Deploy Azure Workbooks
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying Azure workbooks"
+if($deployWorkbooks -eq $true)
+{         
+    $templateBasePath = ".\Workbooks"
+
+    # Get the workbooks ARM template files
+    $workbookCollection = $(ls -Path $templateBasePath | Where-Object {$_.name -like "*.json"})
+        
+    # Deploy all workbooks
+    foreach ($workbook in $workbookCollection)
+    {
+        $workbookName = $($workbook.Name).Split(".")[0]
+        $templateFile = "$templateBasePath\$($workbook.Name)"
+        $deploymentName = $("import_workbook_$workbookName").ToLower()
+
+        # Deploy this workbook
+        Write-Host "Deploying workbook $workbookName"
+        New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+        -workspaceName $MonitorWSName | Out-Null
+            
+    }
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Deploy the Azure dashboard
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying the Azure Dashboard"
+if($deployDashboard -eq $true)
+{
+    $deploymentName = "Deploy_AzureDashboard"
+    $templateFile = ".\Dashboard\deploy.json"
+
+    # Deploy the Azure Dashboard
+    Write-Host "Deploying the Azure Dashboard"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+    -workspaceName $MonitorWSName -location $location | Out-Null    
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Assign Azure Policies
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Assigning required Azure Policies"
+if($deployAzurePolicies -eq $true)
+{        
+    $templateBasePath = ".\Policies"
+    $policiesScope = $parametersFileInput.Scope
+    # Parameter to make unique Microsoft.Authorization/roleAssignments name at tenant level
+    $resourceGroupID = (Get-AzResourceGroup -Name $resourceGroup).ResourceId
+
+    # Get the AzurePolicies ARM template files
+    $azurePoliciesCollection = $(ls -Path $templateBasePath | Where-Object {$_.name -like "*.json"})
+        
+    # Assign the policies
+    foreach ($azurePolicyItem in $azurePoliciesCollection)
+    {
+        $azurePolicyName = $($azurePolicyItem.Name).Split(".")[0]
+        $templateFile = "$templateBasePath\$($azurePolicyItem.Name)"
+        $deploymentName = "AssignPolicy_$($azurePolicyName)".Replace(' ','')
+        $deploymentName = $deploymentName.substring(0, [System.Math]::Min(63, $deploymentName.Length))
+
+        # Assign the policy at resource group/subscription scope
+        Write-Host "Assigning Azure Policy: $azurePolicyName"
+        if($policiesScope -eq "subscription")
+        {
+            New-AzDeployment -Name $deploymentName -location $location -TemplateFile $templateFile `
+            -workspaceName $MonitorWSName -policyAssignmentName $azurePolicyName -resourceGroupID `
+            $resourceGroupID | Out-Null
+        }
+        elseif($policiesScope -eq "resourcegroup") {
+            New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup `
+            -TemplateFile $templateFile -workspaceName $MonitorWSName -location $location `
+            -policyAssignmentName $azurePolicyName -resourceGroupID $resourceGroupID | Out-Null
+        }
+    }
+}
+else {
+    Write-Host "Skipped"
+}
+
+### Deploy Microsoft Defender for Cloud
+Write-Host ""
+Write-Host -ForegroundColor Cyan "Deploying Microsoft Defender for Cloud"
+if($deployDefenderForCloud -eq $true)
+{
+    $templateFile = ".\DefenderForCloud\deploy.json"
+    $templateFileAtSubscription = ".\DefenderForCloud\deployatsubscription.json"
+    $deploymentName = "Deploy_MicrosoftDefenderforCloudresources"
+    $deploymentNameAtSubscription = "Deploy_DefenderForCloudSubscriptionSettings"
+    $emailAddress = $parametersFileInput.Email
+    $securityCollectionTier = $parametersFileInput.securityCollectionTier
+       
+    # Deploy Defender for Cloud
+    Write-Host "Deploying Microsoft Defender for Cloud resources"
+    New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroup -TemplateFile $templateFile `
+    -workspaceName $SecurityWSName -location $location -securityCollectionTier $securityCollectionTier | Out-Null
+
+    # Deployment at subscription level: defender for servers and defender notification settings
+    Write-Host "Deploying Microsoft Defender for Cloud settings at subscription level"
+    New-AzDeployment -Name $deploymentNameAtSubscription -Location $location -TemplateFile $templateFileAtSubscription `
+    -emails $emailAddress | Out-Null    
+}
+else {
+    Write-Host "Skipped"
+}
